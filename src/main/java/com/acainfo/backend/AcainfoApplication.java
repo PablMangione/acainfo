ab76e0cd7ec3dcc33a91ca27c377ea683e36bd48
PabloCalahorro <pablocalahorrojimenez@gmail.com>
2025-08-11 16:52:19 +0200

    Initial commit. Setup and dependencies

---
+++ src/main/java/com/acainfo/backend/AcainfoApplication.java
@@ -0,0 +1,13 @@
+package com.acainfo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcainfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcainfoApplication.class, args);
+    }
+
+}
